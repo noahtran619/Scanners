b6ff6a982334202bfaa5529a53a596b9a9e90ccd
Noah Tran <noah.tran@mrjaffesclass.com>
2024-01-15 22:13:52 -0800

    Initial commit

---
+++ MyScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class  MyScanner
+{
+    public void run() {
+        Scanner scanner = new Scanner(System.in).useDelimiter(",");
+        while (true) {
+            if (scanner.hasNext()) { 
+                String str = scanner.next();
+                System.out.println(str);
+            }
+        }
+    } 
+
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+}
